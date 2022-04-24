6694a46e1cfe90b1e00362601eceb8a9ef2b1e6f
vinaydugyala <80596756+vinaydugyala@users.noreply.github.com>
2022-04-24 12:59:30 +0530

    Add files via upload

---
+++ 3_Implementation/measurement.h
@@ -0,0 +1,7 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 1000000
+#include <util/delay.h>
+#include <stdlib.h>
+#define enable            
+#define registerselection 
---
+++ 3_Implementation/sensor.h
@@ -0,0 +1,9 @@
+#include <avr/io.h>
+#define F_CPU 1000000 
+#include <util/delay.h>
+#define    E  5
+#define RS  6
+
+
+
+
